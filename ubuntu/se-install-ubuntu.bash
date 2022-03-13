b1bdc2cfbce95a86c8ba83272619193ce2ff1f1e
onni00 <samsungnext55@gmail.com>
2022-03-13 16:37:32 +0100

    Update se-install-ubuntu.bash

--- ubuntu/se-install-ubuntu.bash
+++ ubuntu/se-install-ubuntu.bash
@@ -61,7 +61,7 @@ fi
 
 # Download SoftEther | Version 4.34 | Build 9745
 printf "\nDownloading release: ${RED}4.34 RTM${NC} | Build ${RED}9745${NC}\n\n"
-wget -O vpnserver.tar.gz https://www.softether-download.com/files/softether/v4.34-9745-rtm-2020.04.05-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.34-9745-rtm-2020.04.05-linux-x64-64bit.tar.gz
+wget -O vpnserver.tar.gz https://www.softether-download.com/files/softether/v4.38-9760-rtm-2021.08.17-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
 tar -xzf vpnserver.tar.gz
 cd vpnserver
 echo $'1\n1\n1' | make &&
